a0207c6687ce8235912fb0edf1a1a9c3fc1983c6
Séafra <Forder>
2026-02-06 16:54:33 +0100

    bump: 0.3.0 fix: updated code for deepwiki

--- rust/agent-deepwiki/src/main.rs
+++ rust/agent-deepwiki/src/main.rs
@@ -7,8 +7,7 @@ use coral_rs::init_tracing;
 use coral_rs::mcp_server::McpConnectionBuilder;
 use coral_rs::repeating_prompt_stream::repeating_prompt_stream;
 use coral_rs::rig::client::{CompletionClient, ProviderClient};
-use coral_rs::rig::providers::{anthropic};
-use reqwest::header::{AUTHORIZATION, HeaderMap, HeaderValue};
+use coral_rs::rig::providers::anthropic;
 use std::time::Duration;
 
 include!(concat!(env!("OUT_DIR"), "/coral_options.rs"));
@@ -19,27 +18,14 @@ async fn main() {
 
     let options = Options::parse().expect("An error occurred parsing the arguments");
 
-    let coral_mcp = McpConnectionBuilder::build_coral_sse()
+    let coral_mcp = McpConnectionBuilder::build_coral_streamable_http()
         .await
         .expect("Failed to connect to the Coral server");
 
     let devin_mcp = McpConnectionBuilder::builder()
-        .build_sse_with_headers(
-            "https://mcp.devin.ai/sse",
-            HeaderMap::from_iter(
-                [(
-                    AUTHORIZATION,
-                    HeaderValue::from_str(
-                        format!(
-                            "Bearer {}",
-                            std::env::var("DEVIN_API_KEY").expect("DEVIN_API_KEY is required")
-                        )
-                        .as_str(),
-                    )
-                    .expect("DEVIN_API_KEY gave invalid header value"),
-                )]
-                .into_iter(),
-            ),
+        .build_streamable_http_with_auth(
+            "https://mcp.devin.ai/mcp",
+            std::env::var("DEVIN_API_KEY").ok(),
         )
         .await
         .expect("Failed to connect to the Devin MCP server");
@@ -49,27 +35,25 @@ async fn main() {
         .max_tokens(options.max_tokens as u64)
         .build();
 
-    let mut system_prompt = coral_mcp.prompt_with_resources_str(options.system_prompt);
-
-    if let Some(extra_system_prompt) = options.extra_system_prompt {
-        system_prompt = system_prompt.string(extra_system_prompt);
-    }
-
     let claim_manager = ClaimManager::new()
         .mil_input_token_cost(AgentClaimAmount::Usd(1.250))
         .mil_output_token_cost(AgentClaimAmount::Usd(10.000));
 
-    let agent = Agent::new(completion_agent)
-        .preamble(system_prompt)
+    let mut system_prompt = CompletionEvaluatedPrompt::from_string(options.system_prompt)
+        .all_resources(coral_mcp.clone());
+
+    if let Some(extra_system_prompt) = &options.extra_system_prompt {
+        system_prompt = system_prompt.string(extra_system_prompt.to_string())
+    }
+
+    let agent = Agent::new(completion_agent, system_prompt)
         .claim_manager(claim_manager)
         .mcp_server(coral_mcp.clone())
         .mcp_server(devin_mcp);
 
-    let repeating_user_prompt =
-        CompletionEvaluatedPrompt::from_string(options.followup_user_prompt);
-
     let prompt_stream = repeating_prompt_stream(
-        repeating_user_prompt,
+        CompletionEvaluatedPrompt::from_string(options.initial_user_prompt),
+        CompletionEvaluatedPrompt::from_string(options.followup_user_prompt),
         options.iteration_delay.map(Duration::from_secs),
         options.max_iterations as usize,
     );
